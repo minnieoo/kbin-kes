6d3e964ade3cd2991bb7712ac24a89011d76bc93
minnie <minnielovescoffee@gmail.com>
2023-07-14 17:52:42 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.1
+// @version      0.0.2
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -80,8 +80,7 @@ function resizeText() {
     // mag sidebar *variables*
     const magSidebar = document.querySelectorAll('aside#sidebar section.magazine.section');
     const magName = document.querySelectorAll('aside#sidebar section.magazine.section a');
-    const modSidebar = document.querySelectorAll('aside#sidebar .user-list');
-
+    const modSidebar = document.querySelectorAll('section.user-list, section h3');
     // mag side bar *loops*
     magSidebar.forEach(sidebar => {
         sidebar.style.fontSize = fontSizes.magSidebar;
