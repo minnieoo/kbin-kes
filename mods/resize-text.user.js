07de809b245dbc47b4c017c748d66478a787d0de
minnie <minnielovescoffee@gmail.com>
2023-07-14 16:27:28 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -15,30 +15,47 @@
 
 
 function resizeText() {
-    // settings
-    const settingsHeader = getModSettings("optionHeader");
-    const headerFontSize = settSings["optionHeader"];
 
-
-
-
+// === FONT SIZE SETTINGS OBJ === //
+    const fontSizes = {
+        header: `${settings["optionHeader"]}px`,
+        content: `${settings["optionHeader"]}px`,
+        sidebar: `${settings["optionHeader"]}px`
+      };
 
 
 // === HEADER === //
-    // main header elems
+    //header *variables*
     const topHeader = document.querySelectorAll('#header.header'); // selects elem w id header and class header
-    const threadMenu = document.querySelectorAll('menu.options__main'); // 
-    // internal header elems
-    const topHeaderElems = topHeader.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
-    const threadMenuElems = threadMenu.querySelectorAll('li, a');
+    const threadMenu = document.querySelectorAll('menu.options__main'); //
+    const avatar = document.querySelector('img.user-avatar');
+    const avatarWidth = avatar.offsetWidth;
+    const avatarHeight = avatar.offsetHeight;
 
-    // loops
-    topHeaderElems.forEach(headerElem => {
-        headerElem.style.fontSize = `${headerFontSize}pt`;       
-    });
-//end headerResize()
+    // header *loops*
+    topHeader.forEach(headerElem => {
+        const topHeaderElems = headerElem.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
+        
+        topHeaderElems.forEach(resizeHeaderElems => {
+            const percentage = fontSizes.header * 5; // 80% of pix size
+            const newWidth = avatarWidth * (percentage / 100);
+            const newHeight = avatarHeight * (percentage / 100);
 
-}
+            avatar.style.width = `${newWidth}px`;
+            avatar.style.height = `${newHeight}px`;
+
+            resizeHeaderElems.style.fontSize = fontSizes.header;
+        })
+
+    }) 
+
+
+
+
+
+
+
+} // end of resizeText()
 
 
 function textResize(toggle) {
