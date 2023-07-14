93731478476ef83d334556d0007582a5d2197773
minnie <minnielovescoffee@gmail.com>
2023-07-14 14:39:38 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -13,53 +13,51 @@
 // @grant        none
 // ==/UserScript==
 
-function resizeText() {
-    const comment = document.querySelectorAll('blockquote.comment div');
-    const posts = document.querySelectorAll('div.content.formatted');
-    let settings = getModSettings("resize");
-    const sizes = {
-        "Small": ".8rem",
-        "Normal": ".9rem",
-        "Large": "1rem",
-        "Extra Large": "1.1rem",
-        "Extra Extra Large": "1.3rem"
-      };
-    const selectedSize = settings["size"];
-    const sizeValue = sizes[selectedSize];
-    
-    // call functions to modify text sizes depending on application choice
-      if (settings["option"] == "Only comments") {
-          resizeComments();
-      } else if (settings["option"] == "Only posts") {
-          resizePosts();
-      } else if (settings["option"] == "Comments and posts") {
-          resizeComments();
-          resizePosts();
-      }
+
+resizeText() {
+  // settings
+  const settingsHeader = getModSettings("optionHeader");
+  const headerFontSize = settings["optionHeader"]
+
+
   
-    // functions that modify text sizes based on choice
-    function resizeComments() {   
-        function iterateComments(param, size) {
-          param.forEach(comment => {
-            comment.style.fontSize = size;
-          });
-        }
-      
-        iterateComments(comment, sizeValue);
+
+
+
+  // === HEADER === //
+  function headerResize() {
+      // main header elems
+      const topHeader = document.querySelectorAll('#header.header'); // selects elem w id header and class header
+      const threadMenu = document.querySelectorAll('menu.options__main'); // 
+      // internal header elems
+      const topHeaderElems = topHeader.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
+      const threadMenuElems = threadMenu.querySelectorAll('li, a');
+  
+      // loops
+      topHeaderElems.forEach(headerElem => {
+          headerElem.style.fontSize = `${headerFontSize}pt`;       
+      });
+  } //end headerResize()
+
+  
+
+
+
+
+}
+
+
+
+
+function addStep() {
+  const headerStep = document.querySelector('input[kes-key="optionHeader"]');
+  headerStep.setAttribute('steps', '.1');
+}
+
+function textResize(toggle) {
+    if (toggle) {
+        addStep();
+        resizeText();
+    } else {
     }
-  
-    function resizePosts() {
-      posts.forEach(post => {
-        post.style.fontSize = sizeValue;
-      })
-    }
-  }
-  
-  
-  function textResize(toggle) {
-      if (toggle) {
-          resizeText();
-      } else {
-      }
-  }
-  
+}
