05ecdd24cbd68fc34e07fa556dd4008e04568b28
minnie <minnielovescoffee@gmail.com>
2023-07-14 15:39:34 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -14,48 +14,35 @@
 // ==/UserScript==
 
 
-resizeText() {
-  // settings
-  const settingsHeader = getModSettings("optionHeader");
-  const headerFontSize = settings["optionHeader"]
-
-
-  
+function resizeText() {
+    // settings
+    const settingsHeader = getModSettings("optionHeader");
+    const headerFontSize = settSings["optionHeader"];
 
 
 
-  // === HEADER === //
-      // main header elems
-      const topHeader = document.querySelectorAll('#header.header'); // selects elem w id header and class header
-      const threadMenu = document.querySelectorAll('menu.options__main'); // 
-      // internal header elems
-      const topHeaderElems = topHeader.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
-      const threadMenuElems = threadMenu.querySelectorAll('li, a');
-  
-      // loops
-      topHeaderElems.forEach(headerElem => {
-          headerElem.style.fontSize = `${headerFontSize}pt`;       
-      });
-  //end headerResize()
-
-  
 
 
 
+// === HEADER === //
+    // main header elems
+    const topHeader = document.querySelectorAll('#header.header'); // selects elem w id header and class header
+    const threadMenu = document.querySelectorAll('menu.options__main'); // 
+    // internal header elems
+    const topHeaderElems = topHeader.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
+    const threadMenuElems = threadMenu.querySelectorAll('li, a');
+
+    // loops
+    topHeaderElems.forEach(headerElem => {
+        headerElem.style.fontSize = `${headerFontSize}pt`;       
+    });
+//end headerResize()
 
 }
 
-
-
-
-function addStep() {
-  const headerStep = document.querySelector('input[kes-key="optionHeader"]');
-  headerStep.setAttribute('steps', '.1');
-}
 
 function textResize(toggle) {
     if (toggle) {
-        addStep();
         resizeText();
     } else {
     }
