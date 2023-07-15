0a84b1f6c51971cfcc51367ed857e15777418483
minnie <minnielovescoffee@gmail.com>
2023-07-15 13:36:40 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.3.1
+// @version      0.3.2
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -242,11 +242,11 @@ function resizeText() {
         }
         
         document.addEventListener('click', eventListenerCheckbox);
-}
+    }
 
 // === RESET TO DEFAULTS FUNCTIONALITY === //
     if (!eventListenerDefaultButton) {
-        const defaultButton = document.querySelector('label input[kes-key="defaultButton');
+        const defaultButton = document.querySelector('label input[kes-key="defaultButton]');
 
         eventListenerDefaultButton = (e) => {
             if (e.target === defaultButton) {
