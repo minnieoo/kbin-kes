2c5368c6f2d17cdda8cfeb65a6504d1a3e45bd64
minnie <minnielovescoffee@gmail.com>
2023-07-14 21:29:49 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -124,7 +124,7 @@ function resizeText() {
         const sidebarPosts = sidebarPostsElem.querySelectorAll('h3, div.container blockquote.content p, div.container time, div.container a');
 
         sidebarPosts.forEach(sidebarPost => {
-            sidebarPost.style.fontSize = fontSizes.fontSizes.optionProfile;
+            sidebarPost.style.fontSize = fontSizes.optionProfile;
         });
     });
 
