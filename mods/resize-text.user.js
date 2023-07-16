ab8eda6da8e31b59f948cabbe29a81c6d6c42b4a
minnie <minnielovescoffee@gmail.com>
2023-07-15 19:34:12 -0500

    feat: work on resize text script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.5.8
+// @version      0.5.9
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -34,22 +34,25 @@ function resizeText() {
     const topHeader = document.querySelectorAll('#header.header'); // selects elem w id header and class header
     const threadMenu = document.querySelectorAll('menu.options__main'); //
     const avatar = document.querySelector('img.user-avatar');
-    const avatarWidth = avatar.offsetWidth;
-    const avatarHeight = avatar.offsetHeight;
+
 
     // header *loops*
     topHeader.forEach(headerElem => {
         const topHeaderElems = headerElem.querySelectorAll('a img, h1, h2, h3, p, li, span, a:not(.icon), i');
         
         topHeaderElems.forEach(resizeHeaderElems => {
-            const percentage = fontSizes.header * 5; // 80% of pix size
-            const newWidth = avatarWidth * (percentage / 100);
-            const newHeight = avatarHeight * (percentage / 100);
-
-            avatar.style.width = `${newWidth}px`;
-            avatar.style.height = `${newHeight}px`;
-
             resizeHeaderElems.style.fontSize = fontSizes.header;
+            
+            if (avatar) {
+                const avatarWidth = avatar.offsetWidth;
+                const avatarHeight = avatar.offsetHeight;
+                const percentage = fontSizes.header * 5; // 80% of pix size
+                const newWidth = avatarWidth * (percentage / 100);
+                const newHeight = avatarHeight * (percentage / 100);
+
+                avatar.style.width = `${newWidth}px`;
+                avatar.style.height = `${newHeight}px`;
+            }
         })
 
     }) 
@@ -84,6 +87,7 @@ function resizeText() {
 
     // mag sidebar *variables*
     const magSidebar = document.querySelectorAll('aside#sidebar section.magazine.section');
+    const magSidebarName = document.querySelectorAll('aside#sidebar section.magazine.section h3');
     const magName = document.querySelectorAll('aside#sidebar section.magazine.section a');
     const modSidebar = document.querySelectorAll('section.user-list, section.user-list h3');
     // mag side bar *loops*
@@ -98,6 +102,12 @@ function resizeText() {
     modSidebar.forEach(mods => {
         mods.style.fontSize = fontSizes.magSidebar;
     })
+
+    magSidebarName.forEach(magname => {
+        magname.style.fontSize = fontSizes.magSidebar;
+    })
+
+
 
 // === HOMEPAGE SIDEBAR === //
 
@@ -246,44 +256,44 @@ function resizeText() {
 
 // === RESET TO DEFAULTS FUNCTIONALITY === //
 
-    if (!eventListenerDefaultButton) {
-        eventListenerDefaultButton = (e) => {
-        const defaultButton = document.querySelector('input[kes-key="defaultButton"]');
-        const buttonColor = defaultButton.style.backgroundColor;
-        function buttonStyle(button) {
-            button.style.backgroundColor = buttonColor;
-        }
-    
-        if (e.target.type === 'button' && e.target.getAttribute('kes-key') === 'defaultButton') {
-            console.log('Reset defaults button clicked');
-            defaultButton.style.backgroundColor = '#079D0C'
-            setTimeout(() => buttonStyle(defaultButton), 500);
-    
-            const numSelectMain = document.querySelectorAll('input[kes-key^="option"]');
-    
-            numSelectMain.forEach(numSelectElem => {
-            numSelectElem.setAttribute("value", "12");
-            numSelectElem.value = "12";
-            numSelectElem.dispatchEvent(new Event('input')); // Trigger input event to update CSS
-    
-            // Apply updated font size to the corresponding element
-            const optionKey = numSelectElem.getAttribute('kes-key');
-            const elementToUpdate = document.querySelector(`[kes-key="${optionKey}"]`);
-            if (elementToUpdate) {
-                elementToUpdate.style.fontSize = fontSizes[optionKey];
-            }
-            });
-    
-            Object.keys(fontSizes).forEach(key => {
-            console.log(key + ': ' + fontSizes[key])
-            fontSizes[key] = '12px';
-            console.log(`UPDATED: ${key}: ${fontSizes[key]}`)
-            });
-        }
-        }
-    
-        document.addEventListener('click', eventListenerDefaultButton);
+if (!eventListenerDefaultButton) {
+    eventListenerDefaultButton = (e) => {
+      const defaultButton = document.querySelector('input[kes-key="defaultButton"]'); //button
+      const buttonColor = defaultButton.style.backgroundColor;
+      function buttonStyle(button) {
+        button.style.backgroundColor = buttonColor;
+      }
+  
+      if (e.target.type === 'button' && e.target.getAttribute('kes-key') === 'defaultButton') {
+        console.log('Reset defaults button clicked');
+        defaultButton.style.backgroundColor = '#079D0C'
+        setTimeout(() => buttonStyle(defaultButton), 500);
+  
+        const numSelectMain = document.querySelectorAll('input[kes-key^="option"]');
+  
+        numSelectMain.forEach(numSelectElem => {
+          numSelectElem.setAttribute("value", "12");
+          numSelectElem.value = "12";
+          updateState(numSelectElem) // Trigger input event to update CSS
+  
+          // Apply updated font size to the corresponding element
+          const optionKey = numSelectElem.getAttribute('kes-key');
+          const elementToUpdate = document.querySelector(`[kes-key="${optionKey}"]`);
+          if (elementToUpdate) {
+            elementToUpdate.style.fontSize = fontSizes[optionKey];
+          }
+        });
+  
+        Object.keys(fontSizes).forEach(key => {
+          console.log(key + ': ' + fontSizes[key])
+          fontSizes[key] = '12px';
+          console.log(`UPDATED: ${key}: ${fontSizes[key]}`)
+        });
+      }
     }
+  
+    document.addEventListener('click', eventListenerDefaultButton);
+  }
 
 
 } // end of resizeText() function //
