3e314cb6adf07d98b800de0e52c7780436f70c16
minnie <minnielovescoffee@gmail.com>
2023-07-17 22:41:21 -0500

    feat: continue coding resize font script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.7.9
+// @version      0.8.0
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
