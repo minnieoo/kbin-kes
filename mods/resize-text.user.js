14e0b658969a5a446aa4c23b1293e8b0ccefd4db
minnie <minnielovescoffee@gmail.com>
2023-07-17 23:35:48 -0500

    feat: continue coding font size script

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.8.7
+// @version      0.8.8
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -246,6 +246,7 @@ function resizeText() {
 
 // === USER SETTINGS GENERAL === //
     const settingsSizeMultiply = settings["optionUserSettings"] * 1.5;
+    console.log(settingsSizeMultiply);
 
     // user settings general *variables*
     const profileGeneral = document.querySelectorAll('div.container form[name="user_settings"]');
