de9e20ee61b4cda4fdb7564aa534717bca20b7e0
minnie <minnielovescoffee@gmail.com>
2023-07-19 01:41:31 -0500

    chore: experiment

--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -1,4 +1,4 @@
-// @version     0.2.0
+// @version     0.2.1
 
 function navbarIcons (toggle) {
     let settings = getModSettings("nav_icons");
@@ -8,14 +8,9 @@ function navbarIcons (toggle) {
     let font = settings.font
     if (toggle) {
         let selectedFont = '${font}';
-        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '";');
-        document.styleSheets[0].addRule('a[href="/search"] i', 'font-family: "Anton";');
+        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '"; font-family: ' + selectedFont + ';');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content: "' + post + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content: "' + subs + '";');
-
-        const searchText = document.querySelectorAll('a[href="/search"] i');
-        const addText = document.querySelector('header menu li a[aria-label="Add"] i:before');
-        const channelText = document.querySelector('header menu li a[aria-label="Select a channel"] i:before');
     
         searchText.style.setProperty('font-family', 'Anton');
     } else {
