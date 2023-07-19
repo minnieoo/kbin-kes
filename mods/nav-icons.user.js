db8b16108f7522e3cf56a422cf870bb3140f2076
minnie <minnielovescoffee@gmail.com>
2023-07-19 03:02:30 -0500

    chore: exp

--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -7,9 +7,9 @@ function navbarIcons (toggle) {
     let font = settings.font
     let weight = settings.fontWeight
     if (toggle) {
-        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', `content: '${search}'; font-family: '${font}'; font-weight: ${weight};`);
-        document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', `content: '${post}'; font-family: '${font}'; font-weight: ${weight};`);
-        document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', `content: '${subs}'; font-family: '${font}'; font-weight: ${weight};`);
+        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', `content: '${search}'; font-family: '${font}'; font-weight: ${weight * 100};`);
+        document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', `content: '${post}'; font-family: '${font}'; font-weight: ${weight * 100};`);
+        document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', `content: '${subs}'; font-family: '${font}'; font-weight: ${weight * 100};`);
     } else {
         document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content:"\\f002" ; font-family: "Font Awesome 6 Free"; font-weight: initial;');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content:"\+" ; font-family: "Font Awesome 6 Free"; font-weight: initial;');
