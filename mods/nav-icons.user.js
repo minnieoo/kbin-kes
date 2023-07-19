697132772bf391de87bca87ffcb7b8885f606e9a
minnie <minnielovescoffee@gmail.com>
2023-07-18 22:36:58 -0500

    feat: added font changing functionality

--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -3,10 +3,19 @@ function navbarIcons (toggle) {
     let search = settings.search
     let post = settings.post
     let subs = settings.subs
+    let font = settings.font
     if (toggle) {
         document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content: "' + post + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content: "' + subs + '";');
+
+        const searchText = document.querySelector('header menu li a[aria-label="Search"] i::before');
+        const addText = document.querySelector('header menu li a[aria-label="Add"] i::before');
+        const channelText = document.querySelector('header menu li a[aria-label="Select a channel"] i::before');
+    
+        searchText.style.setProperty('font-family', `'${settings.font}'`);
+        addText.style.setProperty('font-family', `'${settings.font}'`);
+        channelText.style.setProperty('font-family', `'${settings.font}'`);
     } else {
         document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content:"\\f002" ;');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content:"\+" ;');
