06703829112eb4ed561130fa6577374f4c477763
minnie <minnielovescoffee@gmail.com>
2023-07-18 23:36:35 -0500

    chore: experiment

--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -1,25 +1,26 @@
-// @version     0.0.6
+// @version     0.0.7
 
 function navbarIcons (toggle) {
     let settings = getModSettings("nav_icons");
     let search = settings.search
     let post = settings.post
     let subs = settings.subs
+    let font = settings.font
     if (toggle) {
         let selectedFont = `font-family: '${settings.font}';`;
-        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '"; font-family: "' + settings.font + '";');
+        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + font + '"; font-family: "' + font + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content: "' + post + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content: "' + subs + '";');
 
-        const searchText = document.querySelector('header menu li a[aria-label="Search"] i:before');
-        const addText = document.querySelector('header menu li a[aria-label="Add"] i:before');
-        const channelText = document.querySelector('header menu li a[aria-label="Select a channel"] i:before');
+        // const searchText = document.querySelector('header menu li a[aria-label="Search"] i:before');
+        // const addText = document.querySelector('header menu li a[aria-label="Add"] i:before');
+        // const channelText = document.querySelector('header menu li a[aria-label="Select a channel"] i:before');
     
-        searchText.style.setProperty('font-family', selectedFont);
-        // addText.style.setProperty('font-family', `'${settings.font};'`);
-        // channelText.style.setProperty('font-family', `'${settings.font};'`);
-        console.log(settings.font);
-        console.log(selectedFont);
+        // searchText.style.setProperty('font-family', selectedFont);
+        // // addText.style.setProperty('font-family', `'${settings.font};'`);
+        // // channelText.style.setProperty('font-family', `'${settings.font};'`);
+        // console.log(settings.font);
+        // console.log(selectedFont);
     } else {
         document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content:"\\f002" ;');
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content:"\+" ;');
