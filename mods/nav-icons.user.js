3cff9a1d92519feda1b900719ee19dcf438d6768
minnie <minnielovescoffee@gmail.com>
2023-07-19 01:47:59 -0500

    chore: experiment

--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -1,4 +1,4 @@
-// @version     0.2.1
+// @version     0.2.2
 
 function navbarIcons (toggle) {
     let settings = getModSettings("nav_icons");
@@ -7,8 +7,8 @@ function navbarIcons (toggle) {
     let subs = settings.subs
     let font = settings.font
     if (toggle) {
-        let selectedFont = '${font}';
-        document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '"; font-family: ' + selectedFont + ';');
+        console.log('font test' + font);
+        document.styleSheets[0].addRule(  'header menu li a[aria-label="Search"] i::before', `content: '${search}'; font-family: '${selectedFont}';`);
         document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content: "' + post + '";');
         document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content: "' + subs + '";');
     
