68b2a16c6e5a7cef499e0fee14de8181bde04c0d
minnie <minnielovescoffee@gmail.com>
2023-07-20 17:28:13 -0500

    f

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Change font size
 // @namespace    https://github.com/aclist
-// @version      0.13.3
+// @version      0.13.4
 // @description  Change the size of comment text.
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -484,8 +484,8 @@ function resizeText() {
     // }
 
     let opacity = settings.opacity;
-    const kesModalContent = document.querySelector('div.kes-settings-modal-content');
-    const kesModalContainer = document.querySelector('div.kes-settings-modal-container');
+    const kesModalContent = document.querySelector('div.kes-settings-modal-content:not(div.kes-settings-modal-helpbox)');
+    const kesModalContainer = document.querySelector('div.kes-settings-modal-container:not(div.kes-settings-modal-helpbox)');
 
     kesModalContent.style.setProperty('opacity', opacity / 10);
     kesModalContainer.style.setProperty('opacity', opacity / 10);
