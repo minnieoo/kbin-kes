d4bf51d71411fe23852cfb3d2a0f6f095efe5c5f
minnie <minnielovescoffee@gmail.com>
2023-07-25 02:59:59 -0500

    feat: add input hover

--- mods/hover-indicator.user.js
+++ mods/hover-indicator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hover Indicator
 // @namespace    https://github.com/aclist
-// @version      0.1
+// @version      0.1.0
 // @description  applies a outline to hovered elements
 // @author       minnieo
 // @match        https://kbin.social/*
@@ -17,7 +17,7 @@ function applyOutlines() {
     const thickness = settings.thickness;
 
     // apply outlines to elements
-    const selectors = 'a:hover, h1:hover, h2:hover, h3:hover, h4:hover, h5:hover, h6:hover, img:hover, button:hover, label:hover, markdown-toolbar:hover, textarea:hover, i:hover, time:hover, small:hover, div.content:hover, ul:hover, li:hover, span:hover, figure:hover';
+    const selectors = 'a:hover, h1:hover, h2:hover, h3:hover, h4:hover, h5:hover, h6:hover, img:hover, button:hover, label:hover, markdown-toolbar:hover, textarea:hover, i:hover, time:hover, small:hover, div.content:hover, ul:hover, li:hover, span:hover, figure:hover, input:hover';
     styleElement = document.createElement('style');
     const outline = `${selectors} { outline: ${thickness}px solid ${color};}`;
     const border = `p:not(div.content p):hover { border: ${thickness}px solid ${color};}`;
